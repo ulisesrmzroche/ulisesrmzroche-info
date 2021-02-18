1a69710e350c7f7bdac6e8b87337a4e9d90207a5
Ulises Ramirez-Roche <ulisesrmzroche@pm.me>
2021-02-18 12:14:39 -0600

    feat: add profile pic

--- src/components/InfoCard/index.js
+++ src/components/InfoCard/index.js
@@ -10,8 +10,8 @@ export default function InfoCard() {
         <h1 className="card-header-title">@ulisesrmzroche</h1>
       </div>
       <div className="card-image">
-        <figure classname="image is-4by3">
-            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+        <figure classname="image is-square">
+            <img src="/profile-pic.jpg" alt="Placeholder image" />
         </figure>
       </div>
       <div className="card-content">
